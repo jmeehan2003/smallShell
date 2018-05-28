87181e0542fe62e22ff945ebd935a9924abb6e21
James Meehan <ghoyas2003@gmail.com>
2018-05-27 22:35:16 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,15 @@
+James Meehan
+CS 344
+README for Project #3: smallsh
+
+*************************************
+To compile smallsh run the following command:
+
+	gcc smallsh.c -o smallsh
+
+To then execute smallsh, type at the command prompt:
+
+	./smallsh
+
+
+
--- smallsh.c
+++ smallsh.c
@@ -9,6 +9,9 @@
 ** process), cd (change directory), and exit.
 *********************************************************************************/
 
+// The lectures and notes were used extensively to create this program. 
+// Where noted, stackoverflow posts were also used to help create some small pieces of the program 
+
 #include <sys/types.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -21,165 +24,73 @@
 
 // global variables
 int argCount  = 0;
+int bgCount = 0;
 int fpONLY = 0;		// flag for foreground processes only
-int bgPID = -5;		
-int bgProcess = 1;
+int bgPID = -5;		// store background pid
+int bgPIDS[100];	// store background pids
+pid_t parent;
 
 void exitStatus(int status);
-
-void parseLine(char *line, char **argv)
-{
-	argCount = 0; 
- 	// until we hit the end of the line
- 	while (*line != '\0') 
-	{       
-		// replace the whitespace with \0
-        	while (*line == ' ' || *line == '\t' || *line == '\n')
-               		*line++ = '\0';    
-	 // save the argument position
-         *argv++ = line;    
-	 argCount++;      
-	
-	 // skipe the argument until
-         while (*line != '\0' && *line != ' ' && *line != '\t' && *line != '\n') 
-               line++;            
-        }
-	// mark the end of the argument list
-        *argv = '\0';                 
-}
-
-/*void catchSIGINT(int signo)
-{
-	char* message = "SIGINT. Use CTRL-Z to Stop. Terminated by signal ";
-	int termSignal = WTERMSIG(signo);
-	int length = snprintf(NULL, 0, "%d", termSignal);
-	char* fullmessage = malloc(49 + length + 1);
-	strcpy(fullmessage,message); 
-	// malloc for length of message (28) plus length of termSignal + 1
-	char* signal= malloc(length + 1);
-	snprintf(signal, length + 1, "%d", termSignal);
-	strcat(fullmessage, signal);
-	strcat(fullmessage, "\n");
-	
-	write(STDOUT_FILENO, fullmessage, 52);
-}*/
-
-void backgroundExit(int status)
-{
-	printf("background pid %d is done: ", bgPID);
-	if(WIFEXITED(status))
-	{
-		int exitStatus = WEXITSTATUS(status);
-		printf("exit value %d\n", exitStatus);
-	}
-	else if (WIFSIGNALED(status))
-	{
-		int termSignal = WTERMSIG(status);
-		printf("terminated by signal %d\n", termSignal);
-	}
-	else
-		printf("HULL BREACH! Uh oh. Process was not exited normally or by a signal!?!\n");
-}
-
-void catchSIGTSTP(int signo)
-{
-	if (!fpONLY)
-	{
-		printf("\nEntering foreground-only mode (& is now ignored)\n: ");
-		fpONLY = 1;
-	}
-	else
-	{
-		printf("Exiting foreground-only mode\n: ");
-		fpONLY = 0;
-	}	
-}
-
-void catchSIGCHLD(int signo)
-{
-	printf("INSIDE SIG CHLD\n");
-	int status;
-	while (waitpid(-1, &status, WNOHANG) > 0){};
-	printf("bgprocess: %d\n", bgProcess);
-	bgProcess = 1;
-	if (bgProcess)
-	{
-		backgroundExit(status);
-		bgProcess = 0;
-	}
-}
-
-void checkFinish()
-{
- 	 int status;
-	 pid_t pid = waitpid(-1, &status, WNOHANG);
-  	 if (pid < 0 && errno != ECHILD)
-    		perror("Waitpid failed");
-      	 else if (pid > 0)
-    		printf("Process %d finished with status %d", pid, status);
-}
+void catchSIGTSTP(int signo);
+void catchSIGQUIT(int signo);
 
 int main()
 {
+	// get pid of parent
+	parent = getpid();
+
+	// create signal handler structs (taken from lecture materials)
+	// create ignore signal handler
 	struct sigaction ignore_action = {0};
 	ignore_action.sa_handler = SIG_IGN;
 
+	// create default signal handler
 	struct sigaction default_action = {0};
 	default_action.sa_handler = SIG_DFL;
 
+	// create handler for SIGINT and set it to default for the parent
 	struct sigaction SIGINT_action = {0};
-//	SIGINT_action.sa_handler = catchSIGINT;
-//	sigfillset(&SIGINT_action.sa_mask);
 	sigaction(SIGINT, &ignore_action, NULL);
-
-/*	struct sigaction SIGCHLD_action = {0};
-	SIGCHLD_action.sa_handler = catchSIGCHLD;
-	sigfillset(&SIGCHLD_action.sa_mask);
-	sigaction(SIGCHLD, &default_action, NULL);
-*/
+	
+	// create signal handler for SIGTSTP.  This will be used to toggle b etween foreground and background modes
 	struct sigaction SIGTSTP_action = {0};
 	SIGTSTP_action.sa_handler = catchSIGTSTP;
 	sigfillset(&SIGTSTP_action.sa_mask);
 	sigaction(SIGTSTP, &SIGTSTP_action, NULL);
 
-	char* args[512];
-	char* userInput = NULL;
+	// create handler for SIGQUIT.  This will terminate all child processes when the user enters "exit"
+	struct sigaction SIGQUIT_action = {0};
+	SIGQUIT_action.sa_handler = catchSIGQUIT;
+	sigfillset(&SIGQUIT_action.sa_mask);
+	sigaction(SIGQUIT, &SIGQUIT_action, NULL);
+
+	// set up/initialize some variables before the while loop starts
+	char* args[512];  // this will hold user entered commands, argument,s and files that will be sent to execvp
+	char* userInput = NULL;  
 	char* outputFile = NULL;
 	char* inputFile = NULL;
-//	int pid = getpid();
-//	printf("%s\n", pid);
-	int ppid = getpid();
-	printf("%d\n", ppid);
-	int length = snprintf(NULL, 0, "%d", ppid);
-	char* strPPID = malloc(length + 1);
-	snprintf(strPPID, length + 1, "%d", ppid);
-	int childExit = 0;
+	int childExit = 0;  // initialize exit status for child
 
-	int run = 1;
-	while (run)
+	// this while loop will run until user enters "exit" or the program is killed
+	while (1)
 	{
-//		sigaction(SIGCHLD, &SIGCHLD_action, NULL);
-		int foreground = 1;
-		int argCount = 0;
-		int inputRedirect = 0;
-		int outputRedirect = 0;
-		memset(args, '\0', sizeof(args));
+		int foreground = 1;  // initialize each process to foreground process to start
+		int argCount = 0;  // holds the number of arguments the user entered
+		int inputRedirect = 0;  // flag to indicate if input redirection is needed
+		int outputRedirect = 0; // falg to indicate if output redirection is needed
+		memset(args, '\0', sizeof(args));  // clear the args array on each iteration
+		
+		// print prompt and flush stdout
+		printf(": ");
+		fflush(stdout);
 
 		int numCharsEntered = -5;
 		size_t bufferSize = 0;
-		char cwd[PATH_MAX];
-		getcwd(cwd, sizeof(cwd));
-//		printf("cdw: %s\n", cwd);
-		if (cwd == NULL)
-		{
-			perror("getcwd() error");
-			fflush(stdout);
-			exit(1);
-		}
+
 		// check for terminated background processes
 		int status;
+		
 		int cpid = waitpid(-1, &status, WNOHANG);
-//		printf("bgpid is: %d\n", bgPID);
 		while(cpid > 0 && cpid == bgPID) 
 		{
 			printf("background process %d is done: ", cpid);
@@ -187,28 +98,21 @@ int main()
 			cpid = waitpid(-1, &status, WNOHANG);
 		}
 
-		int pid = getpid();		
-	//	printf("%d: ", pid);
-		printf(": ");
-		// always flush stdout after printing
-		fflush(stdout);
-		
+	
+		// get line from user.  some extra code here to help getline handle signal interruptoins (from lectures)
 		while(1)
 		{	
 			numCharsEntered = getline(&userInput, &bufferSize, stdin);
-//			printf("numChars: %d\n", numCharsEntered);
 			if (numCharsEntered == -1 )
 				clearerr(stdin);
 			else
 				break;
 		}
-//		printf("Outside getline loop\n");
 		userInput[strcspn(userInput, "\n")] = '\0'; // Remove the trailing \n that getline adds
 	
 		// check is user entered a blank line and go back to start of loop if they did
 		if (numCharsEntered == 1)
 		{
-//			printf("blank line\n");
 			continue;		
 		}
 	
@@ -219,22 +123,43 @@ int main()
 		// if the first characte is #, then the whole line is a comment and is to be ignored
 	        if (str[0] == '#')
 		{		
-//			printf("This line is a comment and is being ignored\n");
 			continue;
 		}
+		// if user enters built-in command "status", display the exit status or terminating signal of the last foreground process
 		else if (strcmp(str, "status") == 0)
 		{
 			exitStatus(childExit);
 		}
-
-	//	printf("First strtok is: %s\n", str);
+		// if user enters built-in command "exit", kill any other running process and then terminate
 		else if (strcmp(str, "exit") == 0)
 		{
+			// terminate all child processes
+			kill(-parent, SIGQUIT);
+			sleep(1); // give program 1 second for children to receive and process signal
+
+			// clean up any background processes
+			cpid = waitpid(-1, &status, WNOHANG);
+			while(cpid > 0 && cpid == bgPID) 
+			{
+				printf("background process %d is done: ", cpid);
+				exitStatus(status);
+				cpid = waitpid(-1, &status, WNOHANG);
+			}
+
+			// clean up
+			free(inputFile);
+			free(outputFile);
+
+			// exit the program
 			printf("Program exiting...\n");
+			fflush(stdout);
 			exit(0);
 		}
+		// if user enters built-in command "cd", change directory to the home directory.  If user enters "cd [name]", change to 
+		// to that directory.  This command accepts relative and absolute paths
 		else if (strcmp(str, "cd") == 0)
 		{
+			// chceck if user supplied any arguments. if not, change to the home directory
 			str = strtok(NULL, " \n");
 			if (str == NULL)
 			{
@@ -242,7 +167,12 @@ int main()
 			}
 			else
 			{
-
+				// check if there is "$$" anywhere and expand to the parent pid if found
+				int shellpid = getpid();
+				int length = snprintf(NULL, 0, "%d", shellpid);
+				char* strPPID = malloc(length + 1);
+				snprintf(strPPID, length + 1, "%d", shellpid);
+				
 				char* ppid = strstr(str,"$$");
 				if (ppid != NULL)
 				{
@@ -259,8 +189,10 @@ int main()
 						strcat(expanded, secondHalf);
 					}
 					str = expanded;
+					free(strPPID);
+					strPPID = NULL;
 				}	
-				//printf("str is: %s\n", str);
+
 				// check if user entered valid absolute path
 				if(chdir(str) == -1)
 				{
@@ -274,7 +206,7 @@ int main()
 					// append relative path to current working directory	
 					strcat(directory, "/");
 					strcat(directory, str);
-				//	printf("directory: %s\n", directory);
+	
 					if(chdir(directory) == -1)
 					{
 						printf("Error: cannot open directory %s \n", str);
@@ -283,10 +215,12 @@ int main()
 				}
 			}
 		}
+		// otherwise user has not entered a bulit-in command.  Store the commands and arguments in an array to be used by a chlid process
 		else
 		{
 			while (str != NULL)
 			{
+				// check if there is input redirection.  store input file name and set input redirect flag
 				if(strcmp(str, "<") == 0)
 				{
 					str = strtok(NULL, " \n");
@@ -294,6 +228,7 @@ int main()
 					inputRedirect = 1;
 					str = strtok(NULL, " \n");
 				} 
+				// check if there is output redirection.  store output file name and set output redirect falg
 				else if(strcmp(str, ">") == 0)
 				{
 					str = strtok(NULL, " \n");
@@ -301,8 +236,10 @@ int main()
 					outputRedirect = 1;
 					str = strtok(NULL, " \n");
 				}
+				// check if this is a background process 
 				else if(strcmp(str, "&") == 0)
 				{
+					// get the next token from strok.  If it is null, then the last word was "&" and it is a background process
 					char* token = strtok(NULL, " \n");
 					if (token == NULL)
 					{
@@ -312,6 +249,7 @@ int main()
 							foreground = 0;
 						}
 					}
+					// else it is not the last word and treat it as normal text
 					else
 					{
 					args[argCount] = malloc(2048 * sizeof(char));
@@ -320,6 +258,7 @@ int main()
 					str = token;
 					}
 				}			
+				// add token to args array
 				if (str != NULL && (strcmp(str, ">") != 0) && (strcmp(str, "<") != 0))
 				{
 					args[argCount] = malloc(2048 * sizeof(char));
@@ -329,20 +268,12 @@ int main()
 				}
 			}
 
-//			printf("last arg is: %s\n", args[argCount - 1]);
-		/*	if (strcmp(args[argCount - 1], "&") == 0)
-			{
-				//printf("backgroudn process started\n");
-				foreground = 0;		
-				//printf("foreground val: %d\n", foreground);
-				strcpy(args[argCount - 1], "\0");
-			}*/
-
-			int i = 0;
-		/*	for(; i < argCount; i++)
+			// DEBUG: print out argument array
+			/*int i = 0;
+			for(; i < argCount; i++)
 				printf("Arg %d: %s   ", i, args[i]);
-			printf("\n");
-		*/	
+			printf("\n");*/
+	
 			pid_t spawnpid;
 			spawnpid = fork();
 			if (spawnpid == -1)
@@ -352,24 +283,24 @@ int main()
 			}
 			else if (spawnpid == 0)
 			{
-			//	printf("inside child. foreground is: %d\n", foreground);
-				//signal( SIGINT, SIG_DFL );
-				//sigaction(SIGCHLD, &ignore_action, NULL);
-	//			printf("FOREGROUND: %d\n", foreground);
+				// if it's a foreground process, change SIGINT handling to its default setting
 				if (foreground)
 				{
 					sigaction(SIGINT, &default_action, NULL); 
 				}
-				else
-				{
-					bgProcess = 1;
-				}
+				sigaction(SIGTSTP, &ignore_action, NULL);
+				
+				// check the args array and expand any "$$" found to the parent pid
 				int i = 0;
 				for(; i < argCount; i++)
 				{
 					char* ppid = strstr(args[i],"$$");
 					if (ppid != NULL)
 					{
+						int shellpid = getppid();
+						int length = snprintf(NULL, 0, "%d", shellpid);
+						char* strPPID = malloc(length + 1);
+						snprintf(strPPID, length + 1, "%d", shellpid);
 						int inputLen = strlen(args[i]);
 						int len = strlen(ppid);
 						char* firstHalf = strndup(args[i], inputLen - len);
@@ -383,16 +314,20 @@ int main()
 							strcat(expanded, secondHalf);
 						}
 						args[i] = expanded;
+						free(strPPID);
+						strPPID = NULL;
 					}	
 						
 				}	
+
+				// if output redirection, set output to user specified file
 				if (outputRedirect)
 				{
 					int targetFD = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 					if (targetFD == -1) 
 					{
 						perror("Target file failed to open");
-							fflush(stdout);
+						fflush(stdout);
 						exit(1);
 					}
 					fcntl(targetFD, F_SETFD, FD_CLOEXEC);
@@ -401,12 +336,13 @@ int main()
 					if (result == -1)
 					{
 						perror("Error with target dup2 command");
-							fflush(stdout);
+						fflush(stdout);
 						exit(2);
 					}
 					
 				}
 
+				// if input redirection, set input to user specified file
 				if (inputRedirect)
 				{	
 					int sourceFD = open(inputFile, O_RDONLY);
@@ -425,13 +361,12 @@ int main()
 					}					
 				}
 				
-				// if background process, redirect output to /dev/null
+				// if background process, and no output file specified, redirect output to /dev/null
 				/*** source: 
 				https://stackoverflow.com/questions/14846768/in-c-how-do-i-redirect-stdout-fileno-to-dev-null-using-dup2-and-then-redirect?
 				***/
 				if (!foreground && outputFile == NULL)
 				{
-			//		printf("inside bckgrd output redirect\n");
 					int devNull = open("/dev/null", O_WRONLY);
 					if (devNull == -1)
 					{
@@ -449,11 +384,10 @@ int main()
 					}
 				}
 				
+				// if background process and no input file specified, redirect input from /dev/null 
 				if (!foreground && inputFile == NULL)
 				{
-			//		printf("inside bckgrd input redirect\n");
-			//		printf("currDir: %s\n", cwd);
-					int currDir = open("junk", O_RDONLY);
+					int currDir = open("/dev/null", O_RDONLY);
 					if (currDir == -1)
 					{
 						perror("Input redirect to current directory failed");
@@ -468,19 +402,15 @@ int main()
 						fflush(stdout);
 						exit(2);
 					}
-					printf("exited input redire\n");
 				}
 
-				
-
-		//		printf("foregrond: %d\n", foreground);
 				if (!foreground)
 				{
-					printf("This should be printing b/c we are in the background mode");
+					printf("This shouldn't be printing b/c we are in the background mode");
 					printf("input file: %s    output file: %s\n", inputFile, outputFile);
 				}	
 
-
+				// run execvp with args array to have command executed
 				if (execvp(*args, args) < 0)
 				{
 					perror("Exec failure! ");
@@ -495,46 +425,52 @@ int main()
 				free(userInput); // Free the memory allocated by getline() or else memory leak
 				userInput = NULL;		
 			
+				// if foreground process, wait for process to terminate.  If process is terminated by a signal, display signal termination.
 				if (foreground)
 				{
 					pid_t childPID = waitpid(spawnpid, &childExit, 0);
-			//		printf("SpawnPID: %d\n", spawnpid);
 					if (WIFSIGNALED(childExit))
 					{
 						int termSignal = WTERMSIG(childExit);
 						printf("terminated by signal %d\n", termSignal);
+						fflush(stdout);
 					}
 				}
 				// in background child
 				else
 				{
 					printf("background pid is %d\n", spawnpid);
+					fflush(stdout);
 					bgPID = spawnpid;
-		//			sigaction(SIGCHLD, &SIGCHLD_action, NULL); 
+					bgPIDS[bgCount] = bgPID;
+					bgCount++; 
 				}
 			
+			// check for any terminated background processes
 			cpid = waitpid(-1, &status, WNOHANG);
-//			printf("bgpid is: %d\n", bgPID);
 			while(cpid > 0 && cpid == bgPID) 
 			{
 				printf("background process %d is done: ", cpid);
 				exitStatus(status);
 				cpid = waitpid(-1, &status, WNOHANG);
 			}
-//			free(outputFile);
-//			free(inputFile);
+		
+
+			// reset output and input files to NULL
 			outputFile = NULL;
 			inputFile = NULL;
-			
 			}
-
 		}	
 	}
-	printf("Exiting from parent\n");
-	return 0;
-	exit(0);	
+	
+	// we should never get here but just in case
+	return 0;	
 }
 
+
+/************************************************************************************************************************
+** Description: exitStatus() returns the exit code or termination signal of the most recently exitied foregroudn function 
+************************************************************************************************************************/
 void exitStatus(int status)
 {	
 	if(WIFEXITED(status))
@@ -549,4 +485,36 @@ void exitStatus(int status)
 	}
 	else
 		printf("HULL BREACH! Uh oh. Process was not exited normally or by a signal!?!\n");
+}
+
+/***************************************************************************************************************
+** Description: catchSIGTSTP() is the signal handler for SIGSTSTP.  When a TSTP signal is received, the mode
+** is switched to foreground-only mode from foreground/background mode or vice versa.
+****************************************************************************************************************/
+void catchSIGTSTP(int signo)
+{
+	if (!fpONLY)
+	{
+		char* message = "\nEntering foreground-only mode (& is now ignored)\n: ";
+		write(STDOUT_FILENO, message, 52);
+		fpONLY = 1;
+	}
+	else
+	{
+		char* message = "\nExiting foreground-only mode\n: ";
+		write(STDOUT_FILENO, message, 32);
+		fpONLY = 0;
+	}	
+}
+
+/***************************************************************************************************************
+** Description: catchSIGQUIT() is th signal handler for SIGQUIT.  This is used in the exit command to clear up
+** all runnign processes.  This will send the quit command to all processes except for the parent.  The parent
+** then terminates itself using the exit command. 
+****************************************************************************************************************/
+void catchSIGQUIT(int signo)
+{
+	pid_t child = getpid();
+	if (parent != child)
+		_exit(0);
 }
